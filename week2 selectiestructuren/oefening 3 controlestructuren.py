c703d509b3f460595fcec4121e8afa3d74845798
stephanieminne14 <stephanie.minne@student.howest.be>
2017-10-02 15:18:56 +0200

    rezr

---
+++ week2 selectiestructuren/oefening 3 controlestructuren.py
@@ -0,0 +1,6 @@
+ #Vraag aan de gebruiker wat zijn geboortejaar is. Indien hij nog geen 18 is, print dan ook een
+#gepaste melding af.
+
+geboortejaar = int(input("Geef je geboortejaar"))
+print("test")
+# if (geboortejaar)
