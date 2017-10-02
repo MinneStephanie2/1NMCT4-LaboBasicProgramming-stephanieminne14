604e49b4c87abab6847ab1d7a577bc16a5a11e91
stephanieminne14 <stephanie.minne@student.howest.be>
2017-10-02 13:36:51 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	week1/oefening 1-3.py
    #	week1/oefening 1.py
    #	week1/oefening 2.py
    #	week1/oefening 5.py
    #	week1/oefening 6.py
    #	week1/oefening 7.py
    #	week1/oefening 8.py

--- week1/oefening 1.py
+++ week1/oefening 1.py
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #symbolen googlen => escape sequence python
 #oefening 1
 print( "stephanie")
 print ("minnne")
 print ( "stephanie.minne@student.howest.be")
-=======
+#=======
 #symbolen googlen => escape sequence python
 #oefening 1
 print( "stephanie")
 print ("minnne")
 print ( "stephanie.minne@student.howest.be")
->>>>>>> origin/master
+#>>>>>>> origin/master
