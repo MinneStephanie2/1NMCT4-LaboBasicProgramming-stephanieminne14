3872db47a3f13ce6cfde015548740527760d3511
stephanieminne14 <stephanie.minne@student.howest.be>
2017-10-05 09:32:16 +0200

    rezr

---
+++ week2 selectiestructuren/werken met functies/oefening 2.py
@@ -0,0 +1,6 @@
+naam = input("geef uw naam")
+# groep = str(input("geef uw groep"))
+
+def printWelkom (naam,groep ="1NMCT1"):
+    print("welkom {0} en jij zit in groep {1}. Heb een fijne dag".format(naam,groep))
+printWelkom(naam)
