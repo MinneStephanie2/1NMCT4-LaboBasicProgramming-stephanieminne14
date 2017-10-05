8b4acefbe8d76785332b48fae0f1a698bec7335f
stephanieminne14 <stephanie.minne@student.howest.be>
2017-10-05 10:08:16 +0200

    rezr

---
+++ week2 selectiestructuren/werken met functies/oefening 4.py
@@ -0,0 +1,9 @@
+a = int( input("geef een getal: "))
+b = int (input("geef een getal: "))
+c = int (input("geef een getal: "))
+
+import math
+def berekenMax (a,b,c):
+    maximum = max(a,b,c)
+    print("Het maximum {0}".format(maximum))
+berekenMax(a,b,c)
