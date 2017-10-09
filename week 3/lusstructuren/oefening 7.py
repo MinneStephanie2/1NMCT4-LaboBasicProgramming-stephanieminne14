5cef03ddf62f4095bb6e4f59f4372f73d03d2fbb
stephanieminne14 <stephanie.minne@student.howest.be>
2017-10-09 15:40:50 +0200

    rezr

--- week 3/lusstructuren/oefening 7.py
+++ week 3/lusstructuren/oefening 7.py
@@ -3,7 +3,7 @@ stapgrootte = int(input("geef de positieve stapgrootte"))
 aantal_getallen =int(input("hoeveel getallen zijn er"))
 
 def print_lijst_getallen (startwaarde, stapgrootte, aantal_getallen):
-
-    for i in range(startwaarde,aantal_getallen,stapgrootte):
+    maxium = stapgrootte*aantal_getallen
+    for i in range(startwaarde,maxium,stapgrootte):
         print("De lijst is: [{0}]".format(i))
 print_lijst_getallen(startwaarde,stapgrootte,aantal_getallen)
