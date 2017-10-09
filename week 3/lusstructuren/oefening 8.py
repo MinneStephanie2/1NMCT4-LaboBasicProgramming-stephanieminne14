f7c91939f3796d92e44270b8e1590258027ea374
stephanieminne14 <stephanie.minne@student.howest.be>
2017-10-09 21:15:22 +0200

    rezr

---
+++ week 3/lusstructuren/oefening 8.py
@@ -0,0 +1,9 @@
+def print_getallen ():
+    einde = 100
+    som = 0
+    optellen = 1
+    while optellen <= einde:
+        som = som + optellen
+        optellen += 1
+    print ("de som is {0}".format(som))
+print_getallen()
---
+++ week 3/lusstructuren/oefening 9.py
@@ -0,0 +1,19 @@
+import random
+gok =0
+getal = random.randrange(0,21,1)
+
+
+def nummer_raden ():
+    gok = int(input("doen een gokje"))
+
+    if (gok > getal):
+        print ("kleiner!")
+    elif(gok < getal):
+        print("grooter!")
+    else:
+        print(getal)
+        print("Goed geraden!")
+while gok != getal:
+    nummer_raden()
+nummer_raden()
+print (getal)
