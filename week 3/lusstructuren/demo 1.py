228a3da4de6e8900e5995306f23c4ff93d7dab6f
stephanieminne14 <stephanie.minne@student.howest.be>
2017-10-09 14:55:15 +0200

    rezr

--- week 3/lusstructuren/demo 1.py
+++ week 3/lusstructuren/demo 1.py
@@ -1,15 +1,25 @@
-# #oneindige lus
-# def oneinige_lus():
-#     i=6
+# # #oneindige lus
+# # def oneinige_lus():
+# #     i=6
+# #
+# # while (i > 0):
+# #     i+=1
+# #     print(i)
 #
-# while (i > 0):
-#     i+=1
-#     print(i)
-
-
-#print 10 veelvouden van een getal
-def print_tien_veelvouden_van(getal):
-    for i in range (0,11):
-        print (i*getal)
-
-print_tien_veelvouden_van(6)
+#
+# #print 10 veelvouden van een getal
+# def print_tien_veelvouden_van(getal):
+#     for i in range (0,11):
+#         print (i*getal)
+#
+# print_tien_veelvouden_van(6)
+#
+# #returne
+# #oppervlakete cirkel
+import math
+def berken_oppervlakte_cirkle (diameter):
+    opp = ((diameter/2)**2) * math.pi
+    return opp
+#de variable oppervlakte wordt het resultaat van de return waarde van mijn functie
+oppervlakte = berken_oppervlakte_cirkle(2)
+print(oppervlakte)
---
+++ week 3/lusstructuren/oefening 2.py
@@ -0,0 +1,2 @@
+def print_lijst_van_getallen ():
+
